1283f2dd08384dfd2979ab2574130702ac4ec6f5
Daniil Korovin <61512362+danilaSADev@users.noreply.github.com>
2022-11-17 15:28:41 +0200

    - created another project with identity authorization

--- RatingRestaurantsApp/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace RatingRestaurantsApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
